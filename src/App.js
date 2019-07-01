a1a19a087757ee0644d13ff564bc39fff417b0e1
Kyle McCloskey <mccloskeykyle@gmail.com>
2019-06-30 21:02:46 -0400

    video addded, didn't commit one file previously

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <React.Fragment>
-
+      <Router>
+        <Switch>
+          
+        </Switch>
+      </Router>
 
     </React.Fragment>
   );
